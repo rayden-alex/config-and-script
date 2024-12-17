c4d32293ee06822a498fab135278782e4fb7dc02
rayden <rayden@tut.by>
2024-12-17 05:44:19 +0300

    When cmd parameter not set then get URL from clipboard

--- yt-dlp/yta.cmd
+++ yt-dlp/yta.cmd
@@ -1,1 +1,7 @@
-yt-dlp.exe --ignore-config --config-locations "yta.conf" %*
+@echo off
+
+IF NOT "[%*]"=="[]" (
+    yt-dlp.exe --ignore-config --config-locations "yta.conf" %*
+) ELSE (
+    for /f "eol=; tokens=*" %%I in ('powershell Get-Clipboard') do yt-dlp.exe --ignore-config --config-locations "yta.conf" %%I
+)
