cb30915c38f67b2dae25010f063e01ff8f42bd08
rayden <rayden@tut.by>
2025-10-17 21:20:55 +0300

    Update scripts

--- yt-dlp/yts.cmd
+++ yt-dlp/yts.cmd
@@ -1,1 +1,7 @@
-yt-dlp.exe --ignore-config --config-locations "yts.conf" %*
+@echo off
+
+IF NOT "[%*]"=="[]" (
+    yt-dlp.exe --ignore-config --config-locations "yts.conf" %*
+) ELSE (
+    for /f "eol=; tokens=*" %%I in ('powershell Get-Clipboard') do yt-dlp.exe --ignore-config --config-locations "yts.conf" %%I
+)
