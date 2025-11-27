4094fd15d5556a09e8d4bafd70c5f282d74980fc
rayden <rayden@tut.by>
2025-11-27 11:03:55 +0300

    Some modifications after Windows reinstall

---
+++ ffmpeg/222.cmd
@@ -0,0 +1,7 @@
+﻿::chcp 65001 > nul
+::ffmpeg  -i infile.flac  -c:v copy  -b:a 320k  outfile.mp3
+ffmpeg  -i "g:\geek-8922622.webm" "g:\geek-8922622.gif"
+pause
+
+::for %%A in (*.flac) do ffmpeg  -i "%%~nA.flac"  -c:v copy  -b:a 320k  "%%~nA.mp3"
+::ffmpeg  -i infile.flac  -q:a 0  outfile.mp3
