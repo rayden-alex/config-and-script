d20199444b08ca26d83823988a2dfbfd4966388d
rayden <rayden@tut.by>
2025-11-27 11:02:35 +0300

    Some modifications after Windows reinstall

--- ffmpeg/vob2mkv.cmd
+++ ffmpeg/vob2mkv.cmd
@@ -3,7 +3,7 @@ set in_file=%1
 set out_opus_file=%in_file:~0,-5%.opus"
 set out_mkv_file=%in_file:~0,-5%.mkv"
 
-c:\Rip\ffmpeg\bin\ffmpeg.exe -hide_banner -i %in_file% -vn -c:a libopus -b:a 192k %out_opus_file%
+c:\Rip\ffmpeg\bin\ffmpeg.exe -hide_banner -i %in_file% -vn -c:a libopus -b:a 192k -mapping_family 1 %out_opus_file%
 
 c:\Rip\MKVToolNix\mkvmerge.exe -o %out_mkv_file% --no-audio %in_file% %out_opus_file%
 
--- yt-dlp/exort_playlist.cmd
+++
@@ -1,1 +0,0 @@
-yt-dlp.exe --skip-download --print "%(duration>%H:%M:%S.%s)s %(creator)s %(uploader)s - %(title)s" https://youtu.be/bJ9r8LMU9bQ
