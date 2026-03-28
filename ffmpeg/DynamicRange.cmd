c0fc2d7ff81be6fdba072e58965b8e25c161b7f6
rayden <rayden@tut.by>
2026-03-28 04:07:33 +0300

    Change symlinks to hardlinks since git doesn't resolve symlink

--- ffmpeg/DynamicRange.cmd
+++ ffmpeg/DynamicRange.cmd
@@ -1,1 +1,34 @@
-C:/Rip/FFMPEG/cmd/DynamicRange.cmd
+@echo off
+Setlocal EnableDelayedExpansion
+set "_ffmpeg=c:\Rip\ffmpeg\bin\ffmpeg.exe -hide_banner "
+
+:: %_ffmpeg% -i "g:\3 - Skylane Drive, Turbo Knight - Half _ Life [audiovk.com].mp3" -af drmeter -f null nul 2>&1 | find "[Parsed_drmeter"
+
+:: To fix error -- "No data, dynamic range not measurable"
+:: increase the filter's length (-af drmeter@length=4). (or more)
+:: length - Set window length in seconds used to split audio into segments of equal length. Default is 3 seconds.
+
+:: Пустые строки в содержимом файлов и выводе команд игнорируются.
+:: Все специальные (служебные) символы необходимо экранировать (предварять птичкой ^).
+:: Это такие символы, как | = ^ < > &. А также % ! - если они используются не для раскрытия переменной, кроме случая с !, когда не включено удаленное расширение переменных.
+:: Сюда же входит знак кавычки, которая соответствует форме кавычки, применяемой для определения вида набора.
+:: Регистр переменной цикла имеет значение.
+:: Изменить вручную значение переменной цикла нельзя. Например, set %%A=param не даст эффекта.
+:: После выхода из цикла переменная цикла уничтожается.
+:: Внутри цикла можно использовать не более 1-го комментария вида :: (иначе, будет критическая ошибка и "вылет" из пакетного файла)
+:: Вместо этого используем Rem.
+:: Внутри цикла можно использовать не более 1-й метки (иначе, будет такой же "вылет").
+:: tokens=2 - значит, что первой букве цикла нужно присвоить значение 2-го токена.
+
+rem FOR /F "tokens=1* delims=]" %%A IN ('%_ffmpeg% -i %* -af drmeter -f null nul 2^>^&1 ^| find "[Parsed_drmeter"') DO (
+rem %_ffmpeg% -i %* -af drmeter@length=5 -f null nul 2>&1 | find "[drmeter"
+
+
+FOR /F "tokens=1* delims=]" %%A IN ('%_ffmpeg% -i %* -af drmeter^=length^=3 -f null nul 2^>^&1 ^| find "[Parsed_drmeter"') DO (
+  set var=%%B
+
+  rem Удаляем первый символ. (Там пробел остался)
+  echo !var:~1!
+)
+
+pause >nul
--- ffmpeg/auCDtect.cmd
+++ ffmpeg/auCDtect.cmd
@@ -1,1 +1,28 @@
-C:/Rip/FFMPEG/cmd/auCDtect.cmd
+@echo off
+chcp 65001 > nul
+
+Setlocal EnableDelayedExpansion
+set "_ffmpeg=c:\Rip\ffmpeg\bin\ffmpeg.exe -hide_banner -loglevel error "
+set "_auCDtect=c:\Rip\auCDtect\auCDtect\auCDtect.exe "
+set "temp_wav=%TEMP%\aucdtect_temp_%RANDOM%.wav"
+
+for %%A in (%*) do (
+  echo.
+  echo [Обработка]: %%~nxA
+
+  %_ffmpeg% -i "%%~A" -vn -af "aresample=44100:resampler=soxr" -sample_fmt s16 -f wav "%temp_wav%"
+  
+  
+  for /f "skip=5 delims=" %%a in ('"%_auCDtect%" -m4 %temp_wav%') do (
+    echo %%a
+  )
+
+  if %errorlevel% neq 0 (
+    echo [Ошибка] Не удалось обработать файл: %%~nxA
+  )
+  
+  del /f /q "%temp_wav%"
+  echo.
+)
+
+pause >nul
